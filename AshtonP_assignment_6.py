5a431692a629b339c0a6c11313efeb26a8680815
Samicoo <pashton7167@gmail.com>
2025-10-07 13:37:10 -0400

    Implimented phone number standardization and proper formatting for email

--- AshtonP_assignment_6.py
+++ AshtonP_assignment_6.py
@@ -7,20 +7,33 @@ while user_input.upper() != "DONE":
     user_input = input("Enter contact information (format: name|phone|email|address): ")
     if user_input.upper() != "DONE":
         string_list = user_input.split("|")
-        name = string_list[0]
-        phone = string_list[1]
-        email = string_list[2]
-        address = string_list[3]
+        unformated_name = string_list[0]
+        unformated_phone = string_list[1]
+        unformated_email = string_list[2]
+        unformated_address = string_list[3]
 
         # Clean up name and address 
-        name = name.strip().title()
-        address = address.strip().title()
+        name = unformated_name.strip().title()
+        address = unformated_address.strip().title()
 
-        contacts_dictionary[count] = {"name":name, "address":address} # Save our new formatted contact into a dictionary to pull from later
+        # Standardize the Phone Number and format email
+        email = unformated_email.lower()
+        digitsOnly = ""
+        for char in unformated_phone:
+            if char.isdigit():
+                digitsOnly += char
+        area_code = "(" + digitsOnly[:3] + ") "
+        number = digitsOnly[3:6] + "-" + digitsOnly[6:]
+        phone = area_code + number
+
+        contacts_dictionary[count] = {"name":name, "address":address, "phone":phone, "email":email} # Save our new formatted contact into a dictionary to pull from later
         count += 1 # increase our incriment
         
 print("=== CONTACT DIRECTORY ===")
 for contact in contacts_dictionary: # Go through each contact in the dictionary
     print(f"CONTACT {contact}")
     print(f"Name: {contacts_dictionary[contact]['name']}")
+    print(f"Phone: {contacts_dictionary[contact]['phone']}")
+    print(f"Email: {contacts_dictionary[contact]['email']}")
     print(f"Address: {contacts_dictionary[contact]['address']}")
+    
